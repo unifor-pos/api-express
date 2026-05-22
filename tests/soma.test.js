eeccf56f3a0f566343d592eec1f6690ed910a07c
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2026-05-22 20:21:35 -0300

    Subindo teste que quebra a CI

--- tests/soma.test.js
+++ tests/soma.test.js
@@ -2,4 +2,9 @@ describe('testar', () => {
     it('testar que 1 + 1 é 2', () => {
         expect(1+1).toBe(2);
     })
+
+    it('testar que 3 * 3 é 10', () => {
+        expect(3 * 3).toBe(10);
+    })
 })
+
